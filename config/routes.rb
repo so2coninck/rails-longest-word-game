d74c3de78d81237a9908e8c52df18eeec09365bf
sophie de coninck <scourbey@gmail.com>
2016-11-21 17:29:56 +0100

    2eme exo

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'game#game'
+  get 'score' => "game#score"
+  get 'game' => "game#game"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
